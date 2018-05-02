c6f4045156c3ad0e8ce724171e1fdca4c7cd67d0
SermusAnton <antonsermus@mail.ru>
2018-05-02 12:17:49 +0300

    Remove Count and remove Add

--- SimpleListArray/ListArray.cs
+++ SimpleListArray/ListArray.cs
@@ -4,30 +4,21 @@ using System.Collections.Generic;
 
 namespace SimpleListArray {
     class CustomList<T>:IEnumerable<T[]> where T : struct {
-        public int Count { get; set; } = 0;
+        public int Count {
+            get {
+                return internalList.Count;
+            }
+        } 
         private List<T[]> internalList = new List<T[]>();
         public void Add() {
             T[] arr = new T[] { default(T) };
-            ++Count;
             internalList.Add(arr);
-        }
-        public void Add( T inValue)   {
-            T[] arr = new T[] {inValue};
-            ++Count;
-            internalList.Add(arr);
-        }
-        public void Add(T inValue1, T inValue2) {
-            T[] arr = new T[] { inValue1, inValue2};
-            ++Count;
-            internalList.Add(arr);
-        }
-        public void Add(T[] arr) {
-            ++Count;
-            internalList.Add(arr);
-        }
+        }      
         public void RemoveAt(int index) {
-            --Count;
             internalList.RemoveAt(index);
+        }
+        public void Add(params T[] arr) {
+            internalList.Add(arr);
         }
 
         public IEnumerator<T[]> GetEnumerator() {
