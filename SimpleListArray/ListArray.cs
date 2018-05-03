44390a2b25f58ee489320c560dcc5a14b96bed2a
SermusAnton <antonsermus@mail.ru>
2018-05-03 13:02:52 +0300

    Add exception for RemoveAt

--- SimpleListArray/ListArray.cs
+++ SimpleListArray/ListArray.cs
@@ -3,19 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 
 namespace SimpleListArray {
-    class CustomList<T>:IEnumerable<T[]> where T : struct {
+    class CustomList<T>:IEnumerable<T[]>, IList<T[]> where T : struct {
         public int Count {
             get {
                 return internalList.Count;
             }
-        } 
+        }
+
+        public bool IsReadOnly => ((IList<T[]>)internalList).IsReadOnly;
+
+        public T[] this[int index] { get => ((IList<T[]>)internalList)[index]; set => ((IList<T[]>)internalList)[index] = value; }
+
         private List<T[]> internalList = new List<T[]>();
         public void Add() {
             T[] arr = new T[] { default(T) };
             internalList.Add(arr);
         }      
         public void RemoveAt(int index) {
-            internalList.RemoveAt(index);
+            if (internalList != null && internalList.Count > 1 && index >= 0 && index < internalList.Count) {
+                internalList.RemoveAt(index);
+            } else {
+                throw new ArgumentOutOfRangeException (@"Значение индекса указано неверно!");
+            }
         }
         public void Add(params T[] arr) {
             internalList.Add(arr);
@@ -27,6 +36,30 @@ namespace SimpleListArray {
 
         IEnumerator IEnumerable.GetEnumerator() {
             return internalList.GetEnumerator();
+        }
+
+        public int IndexOf(T[] item) {
+            return ((IList<T[]>)internalList).IndexOf(item);
+        }
+
+        public void Insert(int index, T[] item) {
+            ((IList<T[]>)internalList).Insert(index, item);
+        }
+
+        public void Clear() {
+            ((IList<T[]>)internalList).Clear();
+        }
+
+        public bool Contains(T[] item) {
+            return ((IList<T[]>)internalList).Contains(item);
+        }
+
+        public void CopyTo(T[][] array, int arrayIndex) {
+            ((IList<T[]>)internalList).CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(T[] item) {
+            return ((IList<T[]>)internalList).Remove(item);
         }
     }
 }
--- SimpleListArray/Program.cs
+++ SimpleListArray/Program.cs
@@ -11,7 +11,7 @@ namespace SimpleListArray
             customLists.Add(10);
             customLists.Add(20,30);
             customLists.Add(new int[] {40,50,60});
-            customLists.RemoveAt(1);
+            customLists.RemoveAt(10);
             Console.WriteLine("Кол-во элементов: " + customLists.Count);
             int k = 0;
             foreach (var customList in customLists) {
